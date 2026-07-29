32c58278923760f5b133b64b2caa852e3304e677
adithya4080 <adithyas712@gmail.com>
2026-07-29 11:33:11 +0530

    Provider dashboard issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
+import { queryClient } from '@/lib/Queryclient';
 import { lazy, Suspense } from "react";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
 import { ProtectedRoute } from "@/components/Protectedroute";
@@ -36,16 +37,6 @@ const ProviderDashboard = lazy(() => import("./pages/ProviderDashboard"));
 const Terms = lazy(() => import("./pages/Terms"))
 const Privacy = lazy(() => import("./pages/Privacy"))
 
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-		staleTime: 1000 * 60 * 5,
-		refetchOnWindowFocus: false,
-		refetchOnMount: false,
-		retry: 1,
-		},
-	},
-});
 
 const App = () => (
 	<ErrorBoundary
